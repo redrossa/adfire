52fd227ba74a15ea995d798d24358ff1f8b8868c
Reno Raksi <adriano.raksi@gmail.com>
2024-09-22 22:24:13 -0500

    Add format types test

--- adfire/format.py
+++ adfire/format.py
@@ -5,9 +5,11 @@ import pandas as pd
 def format_types(record: pd.DataFrame) -> pd.DataFrame:
     return record.astype({'date': 'datetime64[ns]', 'amount': 'float64'})
 
+
 def sort_record(record: pd.DataFrame) -> pd.DataFrame:
     record['amount.asset'] = np.where(record['type'] == 'depository', record['amount'], -record['amount'])
     return record.sort_values(by=['date', 'amount.asset'], ascending=[True, False])
+
 
 def format_record(record: pd.DataFrame) -> pd.DataFrame:
     typed = format_types(record)
