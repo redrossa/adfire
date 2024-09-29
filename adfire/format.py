6cc64fa9203bd2ba05b4b891ee9f40e1df858c6f
Reno Raksi <adriano.raksi@gmail.com>
2024-09-29 15:17:59 -0500

    Updated format tests

--- adfire/format.py
+++ adfire/format.py
@@ -6,6 +6,7 @@ import pandera as pa
 schema = pa.DataFrameSchema({
     'date': pa.Column(pa.DateTime),
     'description': pa.Column(str),
+    'category': pa.Column(str),
     'amount': pa.Column(float),
     'account': pa.Column(str),
     'mask': pa.Column(str),
@@ -14,25 +15,27 @@ schema = pa.DataFrameSchema({
     'balances.current': pa.Column(float, nullable=True),
     'balances.available': pa.Column(float, nullable=True), # ideally nullable only if limit is null
     'balances.limit': pa.Column(float, nullable=True),
-}, coerce=True)
+}, coerce=True, strict='filter')
 
 
-def format_types(record: pd.DataFrame) -> pd.DataFrame:
+def _format_types(record: pd.DataFrame) -> pd.DataFrame:
     return schema.validate(record)
 
 
-def sort_record(record: pd.DataFrame) -> pd.DataFrame:
+def _sort_record(record: pd.DataFrame) -> pd.DataFrame:
     record['amount.asset'] = np.where(record['type'] == 'depository', record['amount'], -record['amount'])
     return record.sort_values(by=['date', 'amount.asset'], ascending=[True, False], ignore_index=True)
 
 
-def fill_balances(record: pd.DataFrame) -> pd.DataFrame:
-    # TODO implement
+def _fill_balances(record: pd.DataFrame) -> pd.DataFrame:
+    record['balances.current'] = record.groupby('account')['amount'].cumsum()
+    record['balances.available'] = record['balances.limit'] - record['balances.current']
     return record
 
 
 def format_record(record: pd.DataFrame) -> pd.DataFrame:
-    typed = format_types(record)
-    sorted = sort_record(typed)
-    filled = fill_balances(sorted)
-    return filled
+    typed = _format_types(record)
+    sorted = _sort_record(typed)
+    filled = _fill_balances(sorted)
+    df = _format_types(filled)
+    return df
---
+++ tests/test_format.py
@@ -0,0 +1,121 @@
+import json
+import os
+
+import numpy as np
+import pandas as pd
+import pandera as pa
+import pytest
+from pandas._testing import assert_frame_equal
+from pandera.errors import SchemaError
+
+from adfire.format import format_record
+from adfire.io import read_record
+
+schema = pa.DataFrameSchema({
+    'date': pa.Column(pa.DateTime),
+    'description': pa.Column(str),
+    'category': pa.Column(str),
+    'amount': pa.Column(float),
+    'account': pa.Column(str),
+    'mask': pa.Column(str),
+    'type': pa.Column(str),
+    'subtype': pa.Column(str),
+    'balances.current': pa.Column(float, nullable=True),
+    'balances.available': pa.Column(float, nullable=True),
+    'balances.limit': pa.Column(float, nullable=True),
+}, coerce=True, strict=True)
+
+
+class Case:
+    cases_path = 'cases/format'
+
+    @classmethod
+    def find_cases(cls):
+        return [os.path.join(Case.cases_path, x) for x in os.listdir(cls.cases_path)]
+
+    @classmethod
+    def load_cases(cls):
+        return [Case(x) for x in Case.find_cases()]
+
+    @classmethod
+    def read_expected_record(cls, path) -> pd.DataFrame:
+        print(path)
+        df = pd.read_csv(path, dtype=str)
+        df = schema(df)
+        return df
+
+    def __init__(self, path):
+        with open(os.path.join(path, 'metadata.json'), 'r') as f:
+            metadata = json.load(f)
+
+        self.name = metadata['name']
+        self.description = metadata['description']
+        self.input = read_record(os.path.join(path, metadata['input']))
+        if 'expected' in metadata:
+            self.expected = Case.read_expected_record(os.path.join(path, metadata['expected']))
+            self.error = None
+        else:
+            match metadata['error']:
+                case 'SchemaError':
+                    self.error = SchemaError
+                    self.expected = None
+                case _:
+                    raise ValueError
+
+
+pytestmark = pytest.mark.parametrize('case', Case.load_cases(), ids=lambda x: x.name)
+
+
+def test_format(case):
+    actual = format_record(case.input)
+    assert_frame_equal(actual, case.expected)
+
+
+def test_types(case):
+    if case.error:
+        with pytest.raises(case.error):
+            format_record(case.input)
+    else:
+        actual = format_record(case.input)
+        schema.validate(actual)
+
+
+def test_date_is_ascending(case):
+    if case.error:
+        with pytest.raises(case.error):
+            format_record(case.input)
+    else:
+        actual = format_record(case.input)
+        computed = actual['date']
+        assert computed.is_monotonic_increasing
+
+
+def test_amount_is_descending_for_equal_dates(case):
+    if case.error:
+        with pytest.raises(case.error):
+            format_record(case.input)
+    else:
+        actual = format_record(case.input)
+        actual['amount.asset'] = np.where(actual['type'] == 'depository', actual['amount'], -actual['amount'])
+        for date, group in actual.groupby('date'):
+            assert group['amount.asset'].is_monotonic_decreasing
+
+
+def test_all_current_balances_filled(case):
+    if case.error:
+        with pytest.raises(case.error):
+            format_record(case.input)
+    else:
+        actual = format_record(case.input)
+        computed = actual['balances.current']
+        assert computed.notna().all()
+
+
+def test_available_balances_are_filled_if_limit_filled(case):
+    if case.error:
+        with pytest.raises(case.error):
+            format_record(case.input)
+    else:
+        actual = format_record(case.input)
+        computed = actual[['balances.available', 'balances.limit']]
+        assert computed.loc[computed['balances.limit'].notna(), 'balances.available'].notna().all()
