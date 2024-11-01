87448341d7e21a81ca1e4865a5d7d43b2e7ae574
Reno Raksi <adriano.raksi@gmail.com>
2024-10-31 19:37:57 -0500

    Update CLI args

--- adfire/main.py
+++ adfire/main.py
@@ -4,11 +4,11 @@ from adfire.adfire import Adfire
 
 parser = argparse.ArgumentParser(description='Adfire CLI')
 parser.add_argument('path', help='the input record path')
-parser.add_argument('-h', '--hash_path', help='the input checksum file')
+parser.add_argument('-k', '--checksum_path', help='the input checksum file')
 parser.add_argument('-o', '--out', help='the formatted record output path')
 
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    adfire = Adfire(args.path, args.hash_path)
+    adfire = Adfire(args.path, args.checksum_path)
     adfire.format(args.out)
