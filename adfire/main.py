079a6ce6cd6ec814ed1aa0f758b4fb55a7a36c55
Reno Raksi <adriano.raksi@gmail.com>
2024-11-03 00:37:20 -0500

    allow CLI to accept multiple file paths to compile

--- adfire/main.py
+++ adfire/main.py
@@ -3,12 +3,12 @@ import argparse
 from adfire.adfire import Adfire
 
 parser = argparse.ArgumentParser(description='Adfire CLI')
-parser.add_argument('path', help='the input record path')
+parser.add_argument('path', nargs='+', help='the input record path')
 parser.add_argument('-k', '--checksum_path', help='the input checksum file')
 parser.add_argument('-o', '--out', help='the formatted record output path')
 
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    adfire = Adfire(args.path, args.checksum_path)
+    adfire = Adfire(*args.path, checksum_path=args.checksum_path)
     adfire.format(args.out)
