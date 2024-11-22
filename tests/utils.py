15018459177403e5d0fc5939ccec7f1d0f41546b
Reno Raksi <adriano.raksi@gmail.com>
2024-11-22 04:56:28 -0600

    Add assert record format function

--- tests/utils.py
+++ tests/utils.py
@@ -4,8 +4,10 @@ from contextlib import contextmanager
 from types import SimpleNamespace
 
 import pandas as pd
+import pandera as pa
 from pandas import testing as tm
 
+from adfire.format import schema
 from adfire.utils import is_id_transaction_match
 
 
@@ -39,8 +41,18 @@ def open_or_none(filename, mode='r'):
             f.close()
 
 
+assertion_schema = pa.DataFrameSchema(schema.columns)
+
+
+def assert_record_format(df: pd.DataFrame):
+    mask_is_posted = df['status'] == 'posted'
+    assert df[mask_is_posted]['id.transaction'].notna().all()
+    assert df[mask_is_posted]['hash'].notna().all()
+    assertion_schema.validate(df)
+
+
 def assert_record_equal(a: pd.DataFrame, b: pd.DataFrame):
     assert is_id_transaction_match(a['id.transaction'], b['id.transaction'])
-    a = a.drop('id.transaction', axis=1)
-    b = b.drop('id.transaction', axis=1)
+    a = a.drop(['id.transaction', 'source', 'hash'], axis=1)
+    b = b.drop(['id.transaction', 'source', 'hash'], axis=1)
     tm.assert_frame_equal(a, b)
