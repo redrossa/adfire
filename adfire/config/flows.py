1cbeb59c9456b086fdf0b16ee2851bafdda6f99e
Reno Raksi <adriano.raksi@gmail.com>
2025-03-20 00:56:18 -0500

    Fixed mixed-up flow labels

--- adfire/config/flows.py
+++ adfire/config/flows.py
@@ -10,6 +10,6 @@ class ChangeSchema(BaseInputSchema):
 class FlowsConfig(BaseConfig):
     def apply_entries(self, entries_df):
         entries_df: pd.DataFrame = ChangeSchema(entries_df)
-        entries_df.loc[entries_df['change'] < 0, 'flow'] = 'credit'
-        entries_df.loc[entries_df['change'] >= 0, 'flow'] = 'debit'
+        entries_df.loc[entries_df['change'] < 0, 'flow'] = 'debit'
+        entries_df.loc[entries_df['change'] >= 0, 'flow'] = 'credit'
         return entries_df
